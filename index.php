29c76e6a8eb8b2bb000dada71803f962a662ac19
xviixviixviixvii <2tsjvvb5by@privaterelay.appleid.com>
2026-05-06 18:27:04 +0300

    2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\MoscowPizzaStore;
 try {
     $store = new MoscowPizzaStore();
-
+    
     echo "--- заказ 1 ---\n";
     $store->orderPizza('cheese');
 
